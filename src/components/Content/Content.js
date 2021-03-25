ad0ec43a5c3a6af8ed68cd092e72451ff8ddc6f5
Elaine Smile <80547818+elainethepain@users.noreply.github.com>
2021-03-25 16:16:16 -0700

    removed h5 tags

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -8,7 +8,7 @@ const Content = () => {
       <Typography variant="h5" component="h5">
         What is a Liquidity Pool?
       </Typography>
-      <Typography variant="p" component="p">
+      <Typography variant="body1" component="p">
         Liquidity is how easily you can turn your cryptocurrency token
         into another token or cash. Generally speaking, people prefer
         more liquidity so they can use their tokens in many ways, and
@@ -17,7 +17,7 @@ const Content = () => {
       <Typography variant="h6" component="h6">
         What is a pool of liquidity?
       </Typography>
-      <Typography variant="p" component="p">
+      <Typography variant="body1" component="p">
         A pool of liquidity is a smart contract where you deposit your
         cryptocurrency token. People depoosit into these smart
         contracts because it helps the token be paired with other
@@ -27,11 +27,11 @@ const Content = () => {
       <Typography variant="h6" component="h6">
         What is a liquidity provider?
       </Typography>
-      <Typography variant="p" component="p">
+      <Typography variant="body1" component="p">
         A liquidity provider (LP) is an individual who deposits their
         tokens into a liquidity pool.
       </Typography>
-      <Typography variant="p" component="p">
+      <Typography variant="body1" component="p">
         .....
       </Typography>
     </div>
